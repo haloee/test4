db46f0cdab2ac1dece3e1851d2ccd6eeb33e4019
Sztaracsek Matyas <sztaracsekmatyas@gmail.com>
2025-04-01 19:29:58 +0200

    PersonRepositorychanges

--- src/main/java/pte/mik/hu/repository/PersonRepository.java
+++ src/main/java/pte/mik/hu/repository/PersonRepository.java
@@ -1,4 +1,16 @@
 package pte.mik.hu.repository;
+import pte.mik.hu.model.Person;
+import java.util.List;
+public class PersonRepository implements ClientRepository<Person> {
+    private final DataSource dataSource = DataSource.getInstance();
 
-public class PersonRepository {
+    @Override
+    public List<Person> listAll() {
+        return dataSource.getAllPersons();
+    }
+
+    @Override
+    public void save(Person client) {
+        dataSource.save(client);
+    }
 }
